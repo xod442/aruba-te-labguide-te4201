035e964ebed2dc3ce733e0f9d113b5bd002d5241
Rick Kauffman <rick@rickkauffman.com>
2024-08-14 14:32:45 -0400

    see file for any changes

--- gen_te4201.sh
+++ gen_te4201.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git add *
+git commit -m 'see file for any changes'
+git push origin main
+
 sudo rm -R $HOME/opt/lab-guides/datacenter/aruba-te-labguide-te4201
 cd $HOME/opt/lab-guides/datacenter/ 
 git clone http://github.com/xod442/aruba-te-labguide-te4201
